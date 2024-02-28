72f7a8bc3192d3ea638c2e92f77ec23226cf67d4
qhdudedi <boyoung035@gmail.com>
2024-02-28 16:14:53 +0900

    StringBuiler 이용

--- back-end-workspace/DatabaseLecture/src/lesson/db/ConnectionManager.java
+++ back-end-workspace/DatabaseLecture/src/lesson/db/ConnectionManager.java
@@ -1,14 +1,41 @@
 package lesson.db;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class ConnectionManager {
     public static Connection getConnection(){
-        return null;
+
+        Connection con = null;
+        String id = "root";
+        String pwd = "1q2w3e4r";
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String jdbcURL = "jdbc:mysql://localhost:3306/gisa";
+
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(jdbcURL, id, pwd);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException se){
+            se.printStackTrace();
+        }
+        return con;
     }
-    public static void closeConnection(ResultSet rs, Statement st, Connection con){
+    public static void closeConnection(ResultSet rs, Statement stmt, Connection con){
+        if(rs != null){
+            try{
+                stmt.close();
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+        if(con != null){
+            try{
+                con.close();
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
 
     }
 }
--- back-end-workspace/DatabaseLecture/src/lesson/db/Test.java
+++ back-end-workspace/DatabaseLecture/src/lesson/db/Test.java
@@ -4,14 +4,29 @@ import java.sql.SQLException;
 
 public class Test {
     public static void main(String[] args) {
-
+        Test test = new Test();
+        test.testStart();
     }
     public void testStart(){
         DAO gisa = new DAO();
         String queryAnswer1 ="";
         String queryAnswer2 ="";
         String queryAnswer3 ="";
-        String queryAnswer4 ="";
+
+        StringBuilder sb = new StringBuilder("select count(*)");
+        sb.append("from");
+        sb.append("(select * from gisa");
+        sb.append("where acc_code='A' or acc_code='B') A");
+        sb.append("where kor+(case");
+        sb.append("when loc_code='A' then 5");
+        sb.append("when loc_code='B' then 10");
+        sb.append("when loc_code='C' then 15");
+        sb.append("end) point");
+        sb.append("form gisa");
+        sb.append("where (eng+math)>=120");
+        String queryAnswer4 =sb.toString();
+
+        System.out.println(queryAnswer4);
 
         try{
             String answer = gisa.selectQuiz1(queryAnswer1);
