6a441ff44520a47969093d07c36758801aa33d81
qhdudedi <boyoung035@gmail.com>
2024-03-22 11:33:29 +0900

    chore :blog 프로젝트 gradle 설정

---
+++ springboot-workspace/blog/src/test/java/sku/lesson/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package sku.lesson.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
