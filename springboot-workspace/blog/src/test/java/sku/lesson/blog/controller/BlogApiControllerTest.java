feaaf55401fe8538d6c2f78b31605b0d3e417c71
qhdudedi <boyoung035@gmail.com>
2024-03-25 15:12:09 +0900

    test : 글 삭제API 테스트

--- springboot-workspace/blog/src/test/java/sku/lesson/blog/controller/BlogApiControllerTest.java
+++ springboot-workspace/blog/src/test/java/sku/lesson/blog/controller/BlogApiControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import sku.lesson.blog.domain.Article;
 import sku.lesson.blog.dto.AddArticleRequest;
+import sku.lesson.blog.dto.UpdateArticleRequest;
 import sku.lesson.blog.repository.BlogRepository;
 
 import java.util.List;
@@ -139,5 +140,36 @@ class BlogApiControllerTest {
         List<Article> articles = blogRepository.findAll();
 
         Assertions.assertThat(articles).isEmpty();
+    }
+    @DisplayName("updateArticle: 블로그 글 수정에 성공한다.")
+    @Test
+    public void updateArticle() throws Exception {
+        // given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        final String newTitle = "new title";
+        final String newContent = "new content";
+
+        UpdateArticleRequest request = new UpdateArticleRequest(newTitle, newContent);
+
+        // when
+        ResultActions result = mockMvc.perform(put(url, savedArticle.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)));
+
+        // then
+        result.andExpect(status().isOk());
+
+        Article article = blogRepository.findById(savedArticle.getId()).get();
+
+        Assertions.assertThat(article.getTitle()).isEqualTo(newTitle);
+        Assertions.assertThat(article.getContent()).isEqualTo(newContent);
     }
 }
