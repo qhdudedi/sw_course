46547c0d74e01a3e73e4fb57a81fb46378d426c8
qhdudedi <boyoung035@gmail.com>
2024-03-26 14:01:07 +0900

    feat: 글 상세보기 뷰 구현

--- springboot-workspace/blog/src/main/java/sku/lesson/blog/controller/BlogViewController.java
+++ springboot-workspace/blog/src/main/java/sku/lesson/blog/controller/BlogViewController.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import sku.lesson.blog.domain.Article;
 import sku.lesson.blog.dto.ArticleListViewResponse;
+import sku.lesson.blog.dto.ArticleViewResponse;
 import sku.lesson.blog.service.BlogService;
 
 import java.util.List;
@@ -21,5 +24,13 @@ public class BlogViewController {
                 .toList();
         model.addAttribute("articles",articleView);
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
