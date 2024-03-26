b3641851920456c0e56c320b92eee93c36b7a233
qhdudedi <boyoung035@gmail.com>
2024-03-26 15:52:15 +0900

    feat: 글 수정 기능 및 뷰 구현

--- springboot-workspace/blog/src/main/java/sku/lesson/blog/controller/BlogViewController.java
+++ springboot-workspace/blog/src/main/java/sku/lesson/blog/controller/BlogViewController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import sku.lesson.blog.domain.Article;
 import sku.lesson.blog.dto.ArticleListViewResponse;
 import sku.lesson.blog.dto.ArticleViewResponse;
@@ -32,5 +33,16 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model){
+        if(id == null){
+            model.addAttribute("article", new ArticleViewResponse());
+        }else{
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
     }
 }
