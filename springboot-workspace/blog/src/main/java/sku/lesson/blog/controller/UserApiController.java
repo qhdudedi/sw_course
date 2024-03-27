5631f4e6641efa1fba02e8e378257d2e3855a4ab
qhdudedi <boyoung035@gmail.com>
2024-03-27 17:21:45 +0900

    feat: 로그아웃 기능 및 뷰 구현

--- springboot-workspace/blog/src/main/java/sku/lesson/blog/controller/UserApiController.java
+++ springboot-workspace/blog/src/main/java/sku/lesson/blog/controller/UserApiController.java
@@ -1,7 +1,12 @@
 package sku.lesson.blog.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import sku.lesson.blog.dto.AddUserRequest;
 import sku.lesson.blog.service.UserService;
@@ -11,10 +16,14 @@ import sku.lesson.blog.service.UserService;
 public class UserApiController {
     private final UserService userService;
 
-
     @PostMapping("/user")
     public String signup(AddUserRequest request){
         userService.save(request);
+        return "redirect:/login";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request,response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
